83e5d430fac530c5abfc024e7758e10274050e20
karmazynow-a <aleks.poreba97@gmail.com>
2020-02-25 17:04:59 +0100

    project update
    working buttons
    layout change
    header change

--- js/app.js
+++ js/app.js
@@ -1,14 +1,80 @@
 
-var matrixSize = 11;
+var matrixSize = 32;
 var startPercent = 50;
 var parB = [4];
 var parS = [3];
 var matrix = new Array(matrixSize).fill(0).map(()=> new Array(matrixSize).fill(0));
 
+
 function onLoad(){
+    //initial values
+    var areBtnsDisabled = false;
+    enableBtns();
+    document.getElementById("startPercent").value = startPercent;
+
     initMatrix();
     paintMatrix();
 	return false;
+}
+
+//start game
+function onStart(){
+    disableBtns();
+    //every one second (?)
+    //make next move
+    //  fill matrix
+    //  paint matrix
+    nextStep();
+    paintMatrix();
+	return false;
+}
+
+//reset matrix
+function onReset(){
+    startPercent = Number(document.getElementById("startPercent").value)
+    console.log("percent value: " + startPercent)
+    initMatrix();
+    paintMatrix();
+	return false;
+}
+
+//stop game
+function onStop(){
+    //stop clock
+    //enable buttons
+    enableBtns();
+	return false;
+}
+
+function onChangeRules(rule){
+    console.log("new rule " + rule);
+    console.log(parS);
+
+    var par = rule[0] == 'S' ? parS : parB;
+
+        if (par.includes(Number(rule[1]))){
+            //remove rule
+            var index = par.indexOf(Number(rule[1]));
+            if (index !== -1) par.splice(index, 1);
+
+            //pop button
+            document.getElementById(rule).style.backgroundColor = "#989ead"
+        }
+        else {
+            par.push(Number(rule[1]));
+            
+            //push button
+            document.getElementById(rule).style.backgroundColor = "#595d66"
+        }
+    
+
+    console.log(parS);
+
+    return false;
+}
+
+function nextStep(){
+
 }
 
 function initMatrix(){
@@ -21,7 +87,7 @@ function initMatrix(){
 }
 
 function paintMatrix(){
-    var mat = document.getElementById("matrix");
+    var mat = document.createElement("matrix");
     
     for (var i = 0; i < matrixSize; ++i){
         var tr = document.createElement('tr');
@@ -33,5 +99,27 @@ function paintMatrix(){
         mat.appendChild(tr);
     }
 
+    document.getElementById("matrix").innerHTML = mat.innerHTML;
+
 	return false;
+}
+
+function disableBtns(){
+    //only stop is enabled - to stop the game
+    areBtnsDisabled = true;
+    document.getElementById("symStart").disabled = true;
+    document.getElementById("symStop").disabled = false;
+    document.getElementById("symReset").disabled = true;
+
+    return false;
+}
+
+function enableBtns(){
+    //only stop is disabled
+    areBtnsDisabled = false;
+    document.getElementById("symStart").disabled = false;
+    document.getElementById("symStop").disabled = true;
+    document.getElementById("symReset").disabled = false;
+    
+    return false;
 }
